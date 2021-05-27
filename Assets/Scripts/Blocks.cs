11b226dac943518cdfe67f77ae4e757309f96a9d
TonTon9 <max.schadrin.2002@yandex.ru>
2021-05-27 21:56:00 +0300

    Добавил префаб снаряда и свет

--- Assets/Scripts/Blocks.cs
+++ Assets/Scripts/Blocks.cs
@@ -6,21 +6,23 @@ public class Blocks : MonoBehaviour
 {
     Transform playerTransform;
     BoxCollider2D boxCollider2D;
+    SpriteRenderer SpriteRenderer;
     void Start()
     {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
         boxCollider2D = GetComponent<BoxCollider2D>();
         boxCollider2D.enabled = false;
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }    
     void FixedUpdate()
     {
-        float posHero = playerTransform.position.y + 0.2f;
+        float posHero = playerTransform.position.y + 0.13f;
         float posBlock = transform.position.y + transform.localScale.y;
-        if (posHero > posBlock)
-        {
+        if (posHero > posBlock)      
             boxCollider2D.enabled = true;
-        }
         else
             boxCollider2D.enabled = false;
+        
+            
     }
 }
